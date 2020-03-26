a0192159970673823e97ec13db33b5f803785ebd
adrian-n_1996 <adrian-n_1996@wp.pl>
2020-03-26 01:56:53 +0100

    poprawki|nav

--- aplikacja/cookbook/recipes/forms.py
+++ aplikacja/cookbook/recipes/forms.py
@@ -1,8 +1,33 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 
 from .models import Recipe
+from django import forms
+from ckeditor.widgets import CKEditorWidget
 
-class RecipeForm(ModelForm):
+class RecipeForm(forms.ModelForm):
+    title = forms.CharField(
+        label = "Tytuł",
+        max_length = 100,
+        required = True,
+    )
+
+    image = forms.FileField(
+        label = "Zdjęcie",
+        required = False,
+    )
+
+    body = forms.CharField(
+        label = "Przepis",
+        widget = CKEditorWidget(),
+        required=False,
+    )
+
+    mini_body = forms.CharField(
+        label = "Krótki opis (widoczny na stronie głownej)",
+        required = False,
+        widget=forms.Textarea(attrs={"rows": 3}),
+    )
+
     class Meta:
         model = Recipe
         fields = ['title', 'image', 'body', 'mini_body']
