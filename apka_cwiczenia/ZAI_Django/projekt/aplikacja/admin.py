3bc049be6074e62d8505f050079ca167c7f3f0f1
9luki <lukzaw09@outlook.com>
2020-04-08 22:17:02 +0200

    Zarejestrowanie modeli

--- apka_cwiczenia/ZAI_Django/projekt/aplikacja/admin.py
+++ apka_cwiczenia/ZAI_Django/projekt/aplikacja/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
-
+from .models import Questions,Choice
 # Register your models here.
 admin.site.register(models.Questions)
+admin.site.register(models.Choice)
