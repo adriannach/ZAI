2dbb8e8b8934c3b8078fb6eb7345adcbfee85499
lukzaw09 <lukzaw09@outlook.com>
2020-04-19 11:52:42 +0200

    Zad 2

--- zadania/zad2.py
+++ zadania/zad2.py
@@ -13,6 +13,31 @@ def funkcja(a_list, b_list):
 
 print(funkcja(list1, list2))
 
+#2
+
+tekst = "lorembladlalvk jdajsna dajdasd"
+
+
+def funkcja_analizujaca_tekst(data_text):
+    lista_liter_tekstu = []
+
+    slownik = {'length': len(data_text), 'letters': lista_liter_tekstu, 'big_letters': 'tekst_WIELIKI',
+               'small_letters': 'tekst_malymi'}
+
+    for i in data_text:
+        lista_liter_tekstu.append(i)
+
+    slownik['letters'] = lista_liter_tekstu
+
+    slownik['big_letters'] = data_text.upper()
+
+    slownik['small_letters'] = data_text.lower()
+
+    return slownik
+
+
+print(funkcja_analizujaca_tekst(tekst))
+
 #3
 def deleteLetters(text, letter):
     text = text.replace(letter, "")
